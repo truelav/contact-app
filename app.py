ce86fbefd2c9b12c7d1a30189d0ad784b5a27560
truelav <madan.serghei@yahoo.com>
2024-10-20 19:49:31 -0400

    ADD | newContact form, newContact handler, successForm.html

--- app.py
+++ app.py
@@ -19,3 +19,18 @@ def contacts():
         contacts_set = Contact.all()
 
     return render_template("index.html", contacts=contacts_set)
+
+@app.route("/contacts/new", methods=['GET'])
+def contacts_new_get():
+    return render_template("new.html", contact=Contact())
+
+@app.route("/contacts/new", methods=['POST'])
+def contacts_new():
+    c = Contact(None, request.form['first_name'], request.form['last_name'], request.form['phone'], request.form['email'])
+    first_name = request.form['first_name']
+    if c.save():
+        # flash("New Contact Created")
+        # redirect("/contacts")
+        return render_template("success.html", name=first_name)
+    else:
+        return render_template("new.html", contact=c)
