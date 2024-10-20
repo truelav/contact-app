d3df1fedccb8669fcebdbe3e1b8216674f28700b
truelav <madan.serghei@yahoo.com>
2024-10-20 19:13:19 -0400

    ADD | contacts.json - db, contacts_model and index/layout.html

--- app.py
+++ app.py
@@ -1,7 +1,21 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template, flash, jsonify, send_file
+from contacts_model import Contact
+import time
+
+Contact.load_db()
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p> Hello World <!p>"
+    return redirect("/contacts")
+
+@app.route("/contacts")
+def contacts():
+    search = request.args.get("q")
+    if search is not None:
+        contacts_set = Contact.search(search)
+    else:
+        contacts_set = Contact.all()
+
+    return render_template("index.html", contacts=contacts_set)
